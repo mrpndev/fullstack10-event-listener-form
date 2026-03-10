19ed2dfdfd8638851985a81ef98b69fe5041db6a
Paul Niemczyk <mrpndev@gmail.com>
2026-03-09 20:55:42 -0400

    event listener beginnings

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("ksadjhfksdahf")
+/* 
+	? Event Listeners
+	* methods which listen for an event
+	* when that event occurs, a callback fx is triggered
+	* that method is called .AddEventListener()
+	
+	? Callbacks
+	* a function that gets called by another function
+	* this call occurs when an event triggers it
+	* event - a string of something that happens
+*/
+
+// 1. Grab an item you're trying to set an event listener on
+let submit = document.querySelector(".submit")
+
+// 2. Apply your event listener and the type of event
+submit.addEventListener("click", event => {
+	console.log(event)
+	event.target.style.color = "red"
+})

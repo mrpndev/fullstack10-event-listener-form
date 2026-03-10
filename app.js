35ed17de99dd40a94963a084617a521301146b2f
Paul Niemczyk <mrpndev@gmail.com>
2026-03-09 20:59:12 -0400

    event listener challenge

--- app.js
+++ app.js
@@ -18,3 +18,15 @@ submit.addEventListener("click", event => {
 	console.log(event)
 	event.target.style.color = "red"
 })
+
+/* 
+	? Big Scary Challenge
+	* create two more elements
+		* input with fullName
+		* button with 'Switch'
+	* create an event listener on the switch button
+	* if the button is clicked and fullName is invisible, show it
+	* if the button is clicked and fullName is visible, hide it
+	
+	! HARD MODE - instead of switch, change it to login/register based on input visibility
+*/

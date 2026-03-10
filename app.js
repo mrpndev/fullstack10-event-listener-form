126f0827e18a7452146d41145ec45bd2d89f6bd7
Paul Niemczyk <mrpndev@gmail.com>
2026-03-10 19:58:19 -0400

    forms finished

--- app.js
+++ app.js
@@ -76,9 +76,10 @@ function handleOutput(msg, el) {
 
 // 2. Apply your event listener and the type of event
 submit.addEventListener("click", event => {
+	// event.preventDefault()
 	// let email = event.target.form[1].value
 	// let pwd = event.target.form[2].value
-
+	console.log(event)
 	let payload = {
 		email: event.target.form[1].value,
 		password: event.target.form[2].value
@@ -86,3 +87,43 @@ submit.addEventListener("click", event => {
 	
 	authenticate(db, payload)
 })
+
+/* 
+	? Forms
+	* they're not just pretty way to ingest data
+	* they're very powerful
+	* they actually want to send a request over to somewhere
+	* this somewhere is guided by the action attribute on the form
+	* any inputs have name attributes
+		* used to define keys or properties
+		* anything filled out in those keys become their values
+	* this changes the URL when we hit the button with type SUBMIT
+
+	? Query Params
+	* part of the URL
+	* defined using key=value pairs
+	* starts with ?
+	* each separated by the &
+	* ex: google.com/email?name=Paul&age=27&isUser=true
+
+	? How to stop a form from submitting?
+	* form attribut of onsubmit="return false;"
+	* change button type from submit to button
+	* on your event listener on that button, run
+		* evt.preventDefault()
+		* this method stops the default form behavior
+*/
+
+// ? Accessing query parameters
+
+let url = document.location
+console.log(url)
+console.log(url.search) // hard to parse out
+
+// ? URLSearchParams interface - helps to parse this data
+let params = new URLSearchParams(url.search)
+// creates a Location object that parses and sanitizes query params
+// these can be accessed and updated using .get() and .set() methods
+let email = params.get("email")
+let pwd = params.get("password")
+console.log(email, pwd)
